5e7df61a22432923cdc41deb7df4234944f8087d
RafaelMQs <rafaeelmq08@gmail.com>
2021-10-22 17:49:56 -0300

    Adicionando o projeto

---
+++ src/main/java/com/projetoSGBD/aula1SGBD/Aula1SgbdApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSGBD.aula1SGBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula1SgbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula1SgbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSGBD/aula1SGBD/Aula1SgbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSGBD.aula1SGBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula1SgbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
